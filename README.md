033a11506daaa66a9cf50176f9ee2b119ca2ad5e
Andrew McWatters <mcwattersandrew@gmail.com>
2016-06-02 13:42:14 -0700

    Added templateUrl support and events

--- README.md
+++ README.md
@@ -15,3 +15,17 @@ definition is matched.
 
 #### Returns
 self
+
+## Events
+
+### routechange
+Dispatched after a route change has happened.
+
+#### Target
+window
+
+### viewcontentloaded
+Dispatched every time the view content is reloaded.
+
+#### Target
+the current element
--- route.js
+++ route.js
@@ -3,24 +3,52 @@
     this.routes = {};
   }
 
-  Route.prototype.when = function(path, handler) {
-    this.routes[path] = handler;
+  Route.prototype.when = function(path, route) {
+    this.routes[path] = route;
     return this;
   };
 
-  Route.prototype.otherwise = function(handler) {
-    this.when(null, handler);
+  Route.prototype.otherwise = function(params) {
+    this.when(null, params);
     return this;
   };
 
   window.route = new Route();
 
+  function getTemplateFor(route, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', route.templateUrl, true);
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        var data = request.responseText;
+        callback(data);
+      }
+    };
+
+    request.send();
+  }
+
+  function onroutechange(route) {
+    if (route.handler) { route.handler(); }
+
+    var event = new CustomEvent('routechange');
+    window.dispatchEvent(event);
+  }
+
   function onpopstate() {
-    var route   = window.route;
-    var routes  = route.routes;
-    var path    = location.pathname;
-    var handler = routes[path] || routes[null];
-    if (handler) { handler(); }
+    var Route  = window.route;
+    var routes = Route.routes;
+    var path   = location.pathname;
+    var route  = routes[path] || routes[null];
+    if (route.templateUrl) {
+      getTemplateFor(route, function(template) {
+        Route.template = template;
+        onroutechange(route);
+      });
+    } else {
+      onroutechange(route);
+    }
   }
 
   window.addEventListener('popstate', onpopstate);
@@ -34,4 +62,16 @@
   }
 
   ready(onpopstate);
+
+  function view() {
+    var router = window.router;
+    var el = document.querySelector('[data-view]');
+    if (el) {
+      el.innerHTML = router.template;
+      var event = new CustomEvent('viewcontentloaded');
+      el.dispatchEvent(event);
+    }
+  }
+
+  window.addEventListener('routechange', view);
 })(window, document, location);
