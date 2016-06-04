015426168980450d7449ee965d62ca57ab84cf3e
Andrew McWatters <mcwattersandrew@gmail.com>
2016-06-04 07:06:05 -0700

    Updated documentation

--- route.js
+++ route.js
@@ -30,7 +30,7 @@
 
     request.onload = function() {
       if (request.status >= 200 && request.status < 400) {
-        callback(request.responseText);
+        callback(this.request);
       }
     };
 
