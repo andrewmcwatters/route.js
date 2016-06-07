47198ab9dd191936ed90f322fa9b80c0c2a3f8f5
Andrew McWatters <mcwattersandrew@gmail.com>
2016-06-07 09:42:28 -0700

    Added example

---
+++ scripts/routes.js
@@ -0,0 +1,10 @@
+route
+  .when('/Book/Moby', {
+    templateUrl: '/views/moby.html'
+  })
+  .when('/Book/Gatsby', {
+    templateUrl: '/views/gatsby.html'
+  })
+  .when('/Book/Scarlet', {
+    templateUrl: '/views/scarlet.html'
+  });
