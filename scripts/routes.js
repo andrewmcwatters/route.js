808b9f6e396d53c57364f3182506b6bf060df0b9
Andrew McWatters <mcwattersandrew@gmail.com>
2016-06-16 08:19:44 -0700

    Fixed #3

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,10 +1,30 @@
+function scope(model, value) {
+  var elements = document.querySelectorAll('[data-bind=\" + model + \"]');
+  for (var i = 0; i < elements.length; i++) { elements[i].textContent = value; }
+}
+
+window.addEventListener('routechange', function() {
+  scope('location.pathname', location.pathname);
+  scope('route.params', JSON.stringify(route.params));
+});
+
+function Book() {
+  scope('name', 'Book');
+  scope('params.bookId', route.params.bookId);
+}
+
+function Chapter() {
+  scope('name', 'Chapter');
+  scope('params.bookId', route.params.bookId);
+  scope('params.chapterId', route.params.chapterId);
+}
+
 route
-  .when('/Book/Moby', {
-    templateUrl: 'views/moby.html'
+  .when('/Book/:bookId', {
+    templateUrl: 'book.html',
+    handler: Book,
   })
-  .when('/Book/Gatsby', {
-    templateUrl: 'views/gatsby.html'
-  })
-  .when('/Book/Scarlet', {
-    templateUrl: 'views/scarlet.html'
+  .when('/Book/:bookId/ch/:chapterId', {
+    templateUrl: 'chapter.html',
+    handler: Chapter
   });
